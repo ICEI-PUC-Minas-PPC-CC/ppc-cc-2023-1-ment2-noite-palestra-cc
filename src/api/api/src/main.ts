461978d327c4c63a0ad66b7089e924720563c011
jp445barbosa <jp445barbosa90@outlook.com>
2023-04-27 16:55:29 -0300

    alterando portas

--- src/api/api/src/main.ts
+++ src/api/api/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(4000);
+  await app.listen(8080);
 }
 
 bootstrap();
