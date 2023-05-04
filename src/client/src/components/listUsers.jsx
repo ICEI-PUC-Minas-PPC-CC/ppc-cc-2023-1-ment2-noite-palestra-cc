95ff3351488e1474b38de8b90a479c8e6e3aec01
jpbarbosa44 <jp445barbosa90@outlook.com>
2023-05-04 00:15:27 -0300

    cria filtro de busca de usuário

--- src/client/src/components/listUsers.jsx
+++ src/client/src/components/listUsers.jsx
@@ -7,7 +7,7 @@ import { Button, IconButton, InputAdornment, TextField, Typography } from '@mui/
 import Box from '@mui/material/Box';
 import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
 import SearchIcon from '@mui/icons-material/Search';
-import Rotas  from '../api';
+import Rotas from '../api';
 
 export function ListUsers() {
   const [usuarios, setUsuarios] = useState([]);
@@ -16,7 +16,7 @@ export function ListUsers() {
   useEffect(() => {
     const getUsers = () => {
       routes.get('/users')
-        .then(response => setUsuarios(response.data ))
+        .then(response => setUsuarios(response.data))
         .catch(error => {
           console.log(error);
         });
@@ -32,7 +32,7 @@ export function ListUsers() {
   }, []);
 
   const deleteUser = (id) => {
-       routes.delete('/users', id)
+    routes.delete('/users', id)
       .then(response => {
         if (response.status === 200) {
           getUsers();
@@ -77,63 +77,64 @@ export function ListUsers() {
 
   return (
     <>
-    <div style={{ marginTop: '3%' }}>
-      <Box
-        sx={{
-          height: '100%',
-          width: '100%',
-          padding: '16px',
-          border: '1px solid #D9D9D9',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          maxWidth: '80%',
-          margin: '0 auto',
-          boxSizing: 'border-box',
-          borderRadius: '5px',
+      <div style={{ marginTop: '3%' }}>
+        <Box
+          sx={{
+            height: '100%',
+            width: '100%',
+            padding: '16px',
+            border: '1px solid #D9D9D9',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            maxWidth: '80%',
+            margin: '0 auto',
+            boxSizing: 'border-box',
+            borderRadius: '5px',
 
-        }}
-      >
-        <div style={{ height: 550, width: '100%', overflow: 'auto' }}>
-        <div>
-          <div style={{ height: 280, width: '100%' }}>
-            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '16px'}}>
-              <Typography variant="h5" component="span" sx={{ mx: 1 }}>
-                Usuários Ativos
-              </Typography>
+          }}
+        >
+          <div style={{ height: 550, width: '100%', overflow: 'auto' }}>
+            <div>
+              <div style={{ height: 280, width: '100%' }}>
+                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '16px' }}>
+                  <Typography variant="h5" component="span" sx={{ mx: 1 }}>
+                    Usuários Ativos
+                  </Typography>
+                </div>
+
+                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '16px', marginTop: '50px' }}>
+                  <TextField fullWidth id="outlined-basic" label="Pesquise o nome do usuário..." variant="outlined" size="small" sx={{
+                    marginRight: '25px'
+                  }} />
+                  <IconButton  type="button" sx={{ p: '10px', marginRight: '25px' }} aria-label="search">
+                      <SearchIcon />
+                    </IconButton>
+                  <Button variant="contained" startIcon={<PersonAddAlt1Icon />} sx={{
+                    backgroundColor: '#00992E',
+                    marginLeft: 'auto'
+                  }}>
+                    Adicionar
+                  </Button>
+                </div>
+
+                <DataGrid
+                  sx={{
+                    marginTop: '3%',
+                    minHeight: '350px'
+
+                  }}
+                  rows={usuarios}
+                  columns={columns}
+                  getRowId={getRowId}
+                  paginationModel={{ page: 1, pageSize: 5 }}
+                  checkboxSelection
+                />
+              </div>
             </div>
-            
-            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '16px', marginTop:'50px' }}>
-            <TextField fullWidth id="outlined-basic" label="Pesquise o nome do usuário..." variant="outlined" size="small" sx={{
-              marginRight: '25px'
-            }}  InputProps={{
-              startAdornment: <InputAdornment position="end"><SearchIcon /></InputAdornment>,
-            }}/>
-              <Button variant="contained" startIcon={<PersonAddAlt1Icon />} sx={{
-                backgroundColor: '#00992E',
-                marginLeft: 'auto'
-              }}>
-                Adicionar
-              </Button>
-            </div>
-            
-            <DataGrid
-              sx={{
-                marginTop: '3%',
-                minHeight: '350px'
-                
-              }}
-              rows={usuarios}
-              columns={columns}
-              getRowId={getRowId}
-              paginationModel={{ page: 1, pageSize: 5 }}
-              checkboxSelection
-            />
           </div>
-          </div>
-        </div>
-      </Box>
-    </div>
-    </>         
+        </Box>
+      </div>
+    </>
   );
 }
