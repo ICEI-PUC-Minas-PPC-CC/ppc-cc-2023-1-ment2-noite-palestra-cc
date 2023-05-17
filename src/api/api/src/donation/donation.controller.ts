590adad71062e6f30227b309f8d740d9e036600a
jpbarbosa44 <jp445barbosa90@outlook.com>
2023-05-17 12:18:01 -0300

    add endpoint filtros por data

--- src/api/api/src/donation/donation.controller.ts
+++ src/api/api/src/donation/donation.controller.ts
@@ -18,4 +18,23 @@ export class DonationController {
   create(@Body() createDonationDto: CreateDonationDto) {
     return this.donationService.createDonation(createDonationDto);
   }
+
+  @Get()
+  listDonation() {
+    return this.donationService.listDonation();
+  }
+
+  @Post('/expiration')
+  async findDonationByExpiration(@Body() body: { date: string }) {
+    const { date } = body;
+    const donation = await this.donationService.findDonationByExpiration(date);
+    return donation;
+  }
+
+  @Post('/entry-date')
+  async findDonationByentryDate(@Body() body: { date: string }) {
+    const { date } = body;
+    const donation = await this.donationService.findDonationByentryDate(date);
+    return donation;
+  }
 }
--- src/api/api/src/donation/donation.service.ts
+++ src/api/api/src/donation/donation.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 import { CreateDonationDto } from './dto/create-donation.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Donation, DonationDocument } from './schema/donation.schema';
-
 @Injectable()
 export class DonationService {
   constructor(
@@ -31,5 +30,38 @@ export class DonationService {
       expirationDate,
     });
     return createdDonation.save();
+  }
+
+  async listDonation() {
+    const allDonations = await this.donationModel.find();
+    return allDonations;
+  }
+
+  async findDonationByType(type: string) {
+    const donation = await this.donationModel.find({ type: type });
+    return donation;
+  }
+
+  async findDonationByExpiration(date: string) {
+    const expirationDate = new Date(date);
+    console.log(expirationDate);
+    const donation = await this.donationModel.find({
+      expirationDate: expirationDate,
+    });
+    return donation;
+  }
+
+  async findDonationByentryDate(date: string) {
+    const entryDate = new Date(date);
+    const donation = await this.donationModel.find({
+      entryDate: entryDate,
+    });
+    return donation;
+  }
+
+  async searcDoantion(letter: string): Promise<Donation[]> {
+    console.log(letter);
+    const regex = new RegExp(`^${letter}`, 'i');
+    return this.donationModel.find({ name: regex }).exec();
   }
 }
