7d974e05b23be51dabba35594995abc6476465fe
Coetti <coettigabriel6@gmail.com>
2023-06-06 22:19:42 -0300

    delete trash
    trash

--- donators/dto/update-donator.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateDonatorDto } from './create-donator.dto';
-
-export class UpdateDonatorDto extends PartialType(CreateDonatorDto) {}
