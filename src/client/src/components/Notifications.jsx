7a380836cc0143fb5a15da639276deef82a8c2ba
adriandamiao <adriandami_@hotmail.com>
2023-06-17 03:34:35 -0300

    Adiciona timeline

--- src/client/src/components/Notifications.jsx
+++ src/client/src/components/Notifications.jsx
@@ -1,31 +1,13 @@
 import React from "react";
-import { BiCamera } from "react-icons/bi";
-import { AiOutlineRedo } from "react-icons/ai";
-import { BsEyeglasses } from "react-icons/bs";
 import notificationStyles from "../css/Notifications.module.css";
+import TimeLine from "./TimeLine";
 
 const Notifications = () => {
   return (
-    
     <div className={notificationStyles.notifications}>
       <h3>Notificações</h3>
-      <div className={notificationStyles.notificationsContent}>
-        {/* Venomentos */}
-        <div className={notificationStyles.venomentos}>
-          <h4>Vencimentos</h4>
-          <div className={notificationStyles.venomentosContent}>
-            <div className={notificationStyles.link}>
-              <div className={notificationStyles.circle}></div>
-              <div className={notificationStyles.line}></div>
-            </div>
-            <div className={notificationStyles.vline}></div>
-            <div className={notificationStyles.redContent}>
-              <p>
-              Alimento Arroz - Perto de vencimento dia 01/04/2023   [ Mensagem sistema]{" "}
-              </p>
-            </div>
-          </div>
-        </div>
+      <div style={{ maxHeight: '800px', overflow: 'auto' }}>
+        <TimeLine />
       </div>
     </div>
   );
---
+++ src/client/src/components/TimeLine.jsx
@@ -0,0 +1,104 @@
+import * as React from 'react';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import ReportProblemIcon from '@mui/icons-material/ReportProblem';
+import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
+import Rotas from '../api';
+import { ArrowDropDownIcon } from '@mui/x-date-pickers';
+import { Fab } from '@mui/material';
+
+import '../css/vertical-load-more.css';
+import { green } from '@mui/material/colors';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import 'dayjs/locale/br';
+import dayjs from 'dayjs';
+
+export default function TimeLine({ days }) {
+  const routes = new Rotas();
+  const [donations, setDonations] = React.useState([]);
+
+  const getAllDonations = () => {
+    routes.get(`/donation/expiring-donations/15`)
+      .then((response) => {
+        setDonations(response.data)
+      })
+  }
+
+  const [indice, setIndice] = React.useState(0);
+  const [listaVazia, setListaVazia] = React.useState([]);
+
+  React.useEffect(() => {
+    loadMore();
+  }, []);
+
+  // const loadMore = () => {
+  //   console.log('Indice: ', indice)
+  //   console.log('lista: ', listaVazia)
+  //   setListaVazia([...listaVazia, ...donations.slice(indice, indice+2)]);
+  //   setIndice(indice+2);
+  // };
+
+  const loadMore = () => {
+    console.log('Indice: ', indice);
+    console.log('lista: ', listaVazia);
+  
+    const startIndex = indice;
+    const endIndex = indice + 2;
+  
+    if (startIndex < donations.length) {
+      const slicedItems = donations.slice(startIndex, endIndex);
+      setListaVazia([...listaVazia, ...slicedItems]);
+  
+      if (slicedItems.length < 2) {
+        // Último elemento não foi adicionado, adicioná-lo manualmente
+        const lastItem = donations[donations.length - 1];
+        setListaVazia([...listaVazia, lastItem]);
+      }
+  
+      setIndice(indice + 2);
+    } else {
+      console.log('Não há mais elementos para adicionar.');
+    }
+  }
+
+  const addButton = () => (
+    <Fab classes={{ root: 'fab-button' }} color="primary" aria-label="add">
+      <ArrowDropDownIcon />
+    </Fab>
+  );
+
+
+
+  React.useEffect(() => {
+    getAllDonations();
+  })
+
+  //intersectionObserverProps={ Object }
+  //Custom props pass to useInView component (default: { rootMargin: '0px 0px 40px 0px' }). See
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <VerticalTimeline>
+        {listaVazia.map(donation => (
+          <VerticalTimelineElement 
+          date={dayjs(donation.expirationDate).format('DD/MM/YYYY')}
+          className= 'vertical-timeline-element--work'
+          contentStyle= {{ background: 'rgb(33, 150, 243)', color: '#000' }}
+          contentArrowStyle= {{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          iconStyle= {{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          >
+            <h3 className="vertical-timeline-element-title">{donation.name}</h3>
+            <h4 className="vertical-timeline-element-title">Quantidade: {donation.amount}</h4>
+          </VerticalTimelineElement>))
+        }
+        
+        <VerticalTimelineElement
+          iconOnClick={loadMore}
+          iconClassName="vertical-timeline-element-icon--button"
+          icon={addButton()}
+        />
+      </VerticalTimeline>
+
+    </div>
+  )
+}
