56ac4a303bf7b9a313d062e7c8bca650e6ac63e6
jpbarbosa44 <jp445barbosa90@outlook.com>
2023-06-23 08:59:13 -0300

    fix

--- src/client/src/components/Welcome.jsx
+++ src/client/src/components/Welcome.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import VolunteerActivismOutlinedIcon from '@mui/icons-material/VolunteerActivismOutlined';
 import { Button, CircularProgress, Stack } from "@mui/material";
+import ReportProblemIcon from '@mui/icons-material/ReportProblem';
+import EqualizerOutlinedIcon from '@mui/icons-material/EqualizerOutlined';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import welcomeStyles from "../css/Welcome.module.css";
 import Rotas from "../api";
 
@@ -9,86 +12,89 @@ const Welcome = () => {
   const [counter, setCounter] = useState(0);
   const [counterValidade, setCounterValidade] = useState(undefined);
   const [configExpiration, setConfigExpiration] = useState(undefined);
+  const [configStock, setConfigStock] = useState(undefined)
   const [stock, setStock] = useState(undefined)
   const [loading, setLoading] = useState(true);
 
-useEffect(() => {
-  getAllDonations()
-  getConfig()
-}, [])
+  useEffect(() => {
+    getAllDonations()
+    getConfig()
+  }, [])
 
-useEffect(() => {
-  if (configExpiration !== undefined) {
-    getAllExpirationDonate()
-  }
-  
-}, [configExpiration])
+  useEffect(() => {
+    if (configExpiration !== undefined) {
+      getAllExpirationDonate()
+    }
+    if (configExpiration !== undefined) {
+      getStockStatus()
+    }
+
+
+  }, [configExpiration])
 
 
   const getConfig = () => {
-      routes.get('/config/648d162134b0f5ccf99f3a1b/find')
-        .then((response) => {
-          setConfigExpiration(response.data.expirationDays);
-          console.log('Config Expiration:', response.data.expirationDays); 
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+    routes.get('/config/648d162134b0f5ccf99f3a1b/find')
+      .then((response) => {
+        setConfigExpiration(response.data.expirationDays);
+        setConfigStock(response.data.stock);
+        console.log('Config Expiration:', response.data.expirationDays);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
 
-  
+
   const getAllDonations = () => {
     return new Promise((resolve, reject) => {
       routes.get("/donation/sum-quantities")
         .then((response) => {
           setCounter(response.data);
-          resolve(); 
+          resolve();
         })
         .catch((error) => {
           console.log(error);
-          reject(error); 
+          reject(error);
         });
     });
   };
-  
-  const getAllExpirationDonate = () => {
-      routes.get(`/donation/expiring-products/${configExpiration}`)
-        .then((res) => {
-          console.log('API Response:', res.data);
-          setCounterValidade(res.data);
-        })
-        .catch((error) => {
-          console.log(error); 
-        })
-        .finally(() => {
-          setLoading(false);
-        });
-  };
 
-    
-  const getStockStatus = () => {
+  const getAllExpirationDonate = () => {
     routes.get(`/donation/expiring-products/${configExpiration}`)
       .then((res) => {
         console.log('API Response:', res.data);
-        setStock(res.data);
+        setCounterValidade(res.data);
       })
       .catch((error) => {
-        console.log(error); 
+        console.log(error);
       })
       .finally(() => {
         setLoading(false);
       });
-};
-  
-    
+  };
+
+
+  const getStockStatus = () => {
+    routes.get(`/donation/isStockBelowMinimum/${configStock}`)
+      .then((res) => {
+        console.log('API Response AQUI:', res.data);
+        setStock(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  };
+
+  console.log(configStock)
+
   return (
     <div className={welcomeStyles.welcome}>
       <div className={welcomeStyles.welcomeNotifications}>
         <div className={welcomeStyles.alimentos}>
           <div className={welcomeStyles.content}>
-            <VolunteerActivismOutlinedIcon sx={{color: "#eb268f"}} />
-
+            <EqualizerOutlinedIcon sx={{ color: "#eb268f" }} />
             <div className={welcomeStyles.text}>
               <h4>Alimentos em estoque</h4>
               <div className={welcomeStyles.counter}>
@@ -102,7 +108,7 @@ useEffect(() => {
           </div>
           <div className={welcomeStyles.notify}>
             <div>
-              <p>Perto da data de vencimento</p>
+              <p>Alimentos próximos da data de vencimento</p>
               {loading ? (
                 <CircularProgress size={20} style={{ color: "#eb268f" }} />
               ) : (
@@ -113,8 +119,17 @@ useEffect(() => {
         </div>
         <div className={welcomeStyles.notify}>
           <div>
-            <p>Estoque mínimo</p>
-            <span>1</span>
+            <p>Situação do estoque</p>
+            <div className={welcomeStyles.content}>
+              {stock ? (
+                <ThumbUpIcon fontSize="large" sx={{ color: '#00A85A' }} />
+              ) : (
+                <div>
+                  <ReportProblemIcon fontSize="large" sx={{ color: '#A80000' }} />
+                  Estoque abaixo do mínimo!
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
